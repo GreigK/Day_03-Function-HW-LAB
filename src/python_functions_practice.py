d811a467f6613d676a65149d47e60e102812a85c
Greig Kerr <greigkerr@virginmedia.com>
2022-07-06 16:34:19 +0100

    HW function

---
+++ src/python_functions_practice.py
@@ -0,0 +1,27 @@
+def return_10():
+    return 10
+
+def add(one, two):
+    return one + two
+    
+def subtract(ten, five):
+    return ten - five
+
+    
+def multiply(four, two):
+    return four * two
+
+def divide(ten, two):
+    return ten/two
+
+def length_of_string(string_length):
+    return len(string_length)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(num1, num2):
+    return int(num1) + int(num2)
+
+def number_to_full_month_name(num1):
+    return num1[1] == "January"
